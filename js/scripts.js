7b070123cab63715b0e7bbf918f77930580de3e8
Radnej <snejatiun@gmail.com>
2021-10-09 23:49:31 +0200

    Update scripts.js
    remove existing code from scripts.js page
    removed/scripts.js
    
    add new code in scripts.js page
    added/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
-alert('Hello world');
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let	pokemonList=[{name: "bulbasaur", height: 0.4, types: ['grass', 'poison']},
+				 {name: "slowpoke", height: 1.1, types: ['psychic', 'water']},
+				 {name: "charizard", height: 1.7, types: ['fire', 'flying']}];
+console.log(pokemonList);
+pokemonList[0].name;
+'bulbasaur'
+pokemonList[0].height;
+0.4
+pokemonList[0].types[0];
+'grass'
+pokemonList[0].types[1];
+'poison'
+pokemonList[1].name;
+'slowpoke'
+pokemonList[1].height;
+1.1
+pokemonList[1].types[0];
+'psychic'
+pokemonList[1].types[1];
+'water'
+pokemonList[2].name;
+'charizard'
+pokemonList[2].height;
+1.7
+pokemonList[2].types[0];
+'fire'
+pokemonList[2].types[1];
+'flying'
