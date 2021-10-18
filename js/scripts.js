8bd97b60e90316ce131830eae9ff3813feef37fd
Radnej <snejatiun@gmail.com>
2021-10-18 19:55:25 +0200

    Update scripts.js
    creat addListItem() in scripts.js page
    created/scripts.js
    
    creat new function called showDetails() in scripts.js page
    created/scripts.js
    
    delet consol.log in scripts.js page
    deleted/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -12,14 +12,27 @@ let pokemonRepository = (function () {
     return pokemonList;
   }
 
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+    button.addEventListener('click', function(event) {
+    showDetails(pokemon);
+});
+  }
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
-console.log(pokemonRepository.getAll()); 
-pokemonRepository.add({ name: '' , heigt: '' , types: ''});
 
+console.log(pokemonRepository.getAll()); 
 pokemonRepository.getAll().forEach(function(pokemon) {
-  console.log( pokemon.name + ' height' + pokemon.height + ' types' + pokemon.types );
+    pokemonRepository.addListItem(pokemon);
+
 });
